07ac1d01dbe586e1b58da81da3a2eaa0994c1dcd
divya gupta <shanu.divya123@gmail.com>
2017-07-23 17:07:20 +0530

    add slider images/text animation

--- main.js
+++ main.js
@@ -7,6 +7,20 @@ var foodieApp = angular.module('foodieApp',[]);
     	pause: 'none'
 	})
 
+	    $(function(){
+    $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+    });
+    
 
 
 
